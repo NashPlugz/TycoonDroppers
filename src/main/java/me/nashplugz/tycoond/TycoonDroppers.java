2ee6d36e6b8d4cda155c39702454a4825548e152
NashPlugz <destinygstatus@gmail.com>
2024-08-20 21:08:59 -0600

    getinstance

--- src/main/java/me/nashplugz/tycoond/TycoonDroppers.java
+++ src/main/java/me/nashplugz/tycoond/TycoonDroppers.java
@@ -54,6 +54,9 @@ public class TycoonDroppers extends JavaPlugin implements Listener, CommandExecu
     }
 
     public static TycoonDroppers getInstance() {
+        if (instance == null) {
+            instance = new TycoonDroppers();
+        }
         return instance;
     }
 
